022cc3fe925ea96e591cce0fe820399c9adbd134
vararatc-cpu <vararatc@unemi.edu.ec>
2025-10-02 16:43:51 -0500

    inicio

--- SupermercadoConsola/Program.cs
+++ SupermercadoConsola/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al supermercado.");
+    }
+}
