649133e6df4fc05dd54c186e1444e609c83a91dc
vararatc-cpu <vararatc@unemi.edu.ec>
2025-10-02 16:48:19 -0500

    producto

---
+++ SupermercadoConsola/Pedido.cs
@@ -0,0 +1,16 @@
+﻿public class Pedido
+{
+    public Producto Producto { get; set; }
+    public int Cantidad { get; set; }
+
+    public Pedido(Producto producto, int cantidad)
+    {
+        Producto = producto;
+        Cantidad = cantidad;
+    }
+
+    public decimal Total()
+    {
+        return Producto.Precio * Cantidad;
+    }
+}
---
+++ SupermercadoConsola/Producto.cs
@@ -0,0 +1,10 @@
+﻿public class Producto
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public Producto(string nombre, decimal precio)
+    {
+        Nombre = nombre;
+        Precio = precio;
+    }
+}
